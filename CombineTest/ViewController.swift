a9179195da697e1601eac0f81a5b45a549eb707c
Vitalii Kizlov <Vitalii.Kizlov@deltatre.com>
2020-07-06 13:45:50 +0300

    Initial Commit

---
+++ CombineTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CombineTest
+//
+//  Created by Vitalii Kizlov on 06.07.2020.
+//  Copyright © 2020 Vitalii Kizlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
